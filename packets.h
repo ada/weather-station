6a60f202fd8e3665864002f7523387b623a27293
Johan <johaa1993@gmail.com>
2016-05-22 15:26:57 +0200

    update

---
+++ packets.h
@@ -0,0 +1,33 @@
+#ifndef Packets_H
+#define Packets_H
+
+
+#define Packet_Kind_Message 'M'
+#define Packet_Kind_Response 'R'
+#define Packet_Kind_Float 'f'
+#define Packet_Kind_Int 'i'
+
+#define Packet_Size 16
+#define Packet_Message_Size 15
+
+struct Packet
+{
+  union
+  {
+    char Data [Packet_Size];
+    struct
+    {
+      char Kind;
+      union
+      {
+        char Message [Packet_Message_Size];
+        int Value_Int;
+        float Value_Float;
+      };
+    };
+  };
+};
+
+
+
+#endif
