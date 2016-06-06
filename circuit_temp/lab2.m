183f21f7b55417076798d1c0d6579d1a41c25c27
Alex Anis <aan12003@gmail.com>
2016-06-06 17:38:43 +0200

    temperature with thermistor

--- circuit_temp/lab2.m
+++ circuit_temp/lab2.m
@@ -1,12 +1,13 @@
 clc; clear all; close all; 
-d = csvread('lab2-data.csv');
-
+%d = csvread('lab2-data.csv');
+d = csvread('lab2-data-thermistor.csv');
 figure;
 temp = d(:,1);
 volt = d(:,2);
 
-k = polyfit(volt, temp, 2);
-fprintf('%0.4f %0.4f %0.4f\n', k(3), k(2), k(1));
+k = polyfit(volt, temp,3);
+%fprintf('%0.4f %0.4f\n', k(2), k(1));
+fprintf('%0.4f %0.4f %0.4f %0.4f\n', k(4), k(3), k(2), k(1));
 
 x = linspace(min(volt), max(volt), 100); 
 temperatureFit = polyval(k, x); 
